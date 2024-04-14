a061840bd680b92f32275b75d006a5477f876efd
Michael <mpeder75@gmail.com>
2024-04-14 16:00:46 +0200

    Session 9 update

--- Session 9 - AfleveringV2/Program.cs
+++ Session 9 - AfleveringV2/Program.cs
@@ -11,7 +11,7 @@ namespace Session_9___AfleveringV2
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new AppStart());
         }
     }
 }
